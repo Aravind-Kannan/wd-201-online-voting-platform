7e567256d29d26bea894be26b36ead22cb9403fe
Aravind Kannan Rathinasabapathi <aravindkannan2001@gmail.com>
2022-12-19 21:21:28 +0530

    :card_file_box: chore: create models related to admin

---
+++ models/elections.js
@@ -0,0 +1,32 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Elections extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Elections.belongsTo(models.Users, {
+        foreignKey: "userId",
+      });
+      Elections.hasMany(models.Questions, {
+        foreignKey: "electionId",
+      });
+      Elections.hasMany(models.Voters, {
+        foreignKey: "electionId",
+      });
+    }
+  }
+  Elections.init(
+    {
+      name: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Elections",
+    }
+  );
+  return Elections;
+};
---
+++ models/options.js
@@ -0,0 +1,26 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Options extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Options.belongsTo(models.Questions, {
+        foreignKey: "questionId",
+      });
+    }
+  }
+  Options.init(
+    {
+      title: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Options",
+    }
+  );
+  return Options;
+};
